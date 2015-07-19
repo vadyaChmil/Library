ba1086f6e6f15300f8b7670f3cf025963cafc92e
vadyaChmil <vadya.chmil@gmail.com>
2015-07-19 09:54:28 +0300

    add Spring JDBC

--- src/main/java/vadya_zakusylo/library/servlets/HttpServletLibrary.java
+++ src/main/java/vadya_zakusylo/library/servlets/HttpServletLibrary.java
@@ -1,11 +1,14 @@
 package vadya_zakusylo.library.servlets;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+import javax.sql.DataSource;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class HttpServletLibrary extends HttpServlet {
 
@@ -13,9 +16,6 @@ public class HttpServletLibrary extends HttpServlet {
 	 * Vadya Zakusylo
 	 */
 	private static final long serialVersionUID = 1L;
-
-	public static final String USER_NAME = "adminY5AC8vj";
-	public static final String USER_PASSWORD = "nIz7xbx1XU9t";
 
 	public static final String CONNECTION = "connection";
 	public static final String BOOKLIST = "booksList";
@@ -28,19 +28,13 @@ public class HttpServletLibrary extends HttpServlet {
 	public static final String INSERT_BOOKS_PAGE = "insert_books.jsp";
 	public static final String ERROR_PAGE = "error.jsp";
 
-	static {
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-		} catch (ClassNotFoundException e) {
-			throw new RuntimeException(e);
-		}
-	}
-
 	Connection getConnection(HttpServletRequest request) {
 		Connection connection = (Connection) request.getSession().getAttribute(CONNECTION);
 		if (connection == null) {
 			try {
-				connection = DriverManager.getConnection("jdbc:mysql://127.6.166.130:3306/vadya", USER_NAME, USER_PASSWORD);
+				ApplicationContext context = new ClassPathXmlApplicationContext("Spring-Datasource.xml");
+				DataSource dataSource = (DataSource) context.getBean("dataSource");
+				connection = dataSource.getConnection();
 				request.getSession().setAttribute(CONNECTION, connection);
 			} catch (SQLException e) {
 				throw new RuntimeException(e);
